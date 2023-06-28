287da8c346bae7d3b725ae0c9049f696ada2a286
xxGrinkorxx <137322932+xxGrinkorxx@users.noreply.github.com>
2023-06-28 14:34:33 +0300

    Add recipe to refill gas mask filters
    Gas mask filters can be refilled with activated carbon tablets

--- src/clothing_masks.hpp
+++ src/clothing_masks.hpp
@@ -8,7 +8,7 @@ class GasMask_Filter: Inventory_Base
     radiationProtection = 0.29;
 };
 
-class GasMask_Filter_Improvised
+class GasMask_Filter_Improvised: GasMask_Filter
 {
     radiationProtection = 0.29;
 };
